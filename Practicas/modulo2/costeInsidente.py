d5508d4fc78bca0a818330f17fe8e04cdaddbd97
Luis08089 <fernandomartinez@morpheusdss.com>
2022-06-14 09:12:33 -0500

    cambios

--- Practicas/modulo2/costeInsidente.py
+++ Practicas/modulo2/costeInsidente.py
@@ -4,6 +4,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 #Generación del conjunto de datos 
 x = 2 * np.random.rand(100,1)
@@ -34,4 +35,31 @@ df['coste'] = df['coste'].astype('int')
 plt.plot(df['n_equipos_afectados'], df['coste'], 'b.')
 plt.xlabel('Equipos afectados')
 plt.ylabel('Coste del incidente')
+#plt.show()
+
+# Construcción del modelo 
+#Construcción del modelo y ajuste de la función hipótesis
+lin_reg = LinearRegression()
+lin_reg.fit(df['n_equipos_afectados'].values.reshape(-1,1), df['coste'].values)
+
+# Parámetro theta 0
+print(lin_reg.intercept_)
+# Parámetro theta 1
+print(lin_reg.coef_)
+
+# Predicción para el valor mínimo y máximo del conjunto de datos de entrenamiento
+X_min_max = np.array([[df['n_equipos_afectados'].min()], [df['n_equipos_afectados'].max()]])
+Y_train_pred = lin_reg.predict(X_min_max)
+
+# Representación gráfica de la función hipótesis generada
+plt.plot(X_min_max, Y_train_pred,'g-')
+plt.plot(df['n_equipos_afectados'], df['coste'],'b.')
+plt.xlabel('Equipos afectados')
+plt.ylabel('Coste del incidente')
+plt.grid(True)
 plt.show()
+
+# Predicción de nuevos ejemplos
+x_new = np.array([[1200]])
+coste = lin_reg.predict(x_new)
+print('El coste del incidente sería:', int(coste[0]), "$")
